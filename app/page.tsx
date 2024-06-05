d4f4f72a9be39b76f64e8e2eb85e7c3d2d285dc2
Singgih <gokonggokong71@gmail.com>
2024-06-05 22:39:16 +0700

    add sign-in and sign-up clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Page() {
   return (
-    <Button size={'lg'} variant={"destructive"}>Click Me</Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   );
 }
