081026f6476ebbce00e41d1c4265d96f7b023d9c
Singgih <gokonggokong71@gmail.com>
2024-06-10 21:33:47 +0700

    add eventType user.created, user.updated, and user.deleted

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { db } from '@/lib/db'
 
 export async function POST(req: Request) {
 
@@ -42,16 +43,53 @@ export async function POST(req: Request) {
     } catch (err) {
         console.error('Error verifying webhook:', err);
         return new Response('Error occured', {
-        status: 400
-        })
+            status: 400
+        });
     }
 
     // Do something with the payload
     // For this guide, you simply log the payload to the console
-    const { id } = evt.data;
     const eventType = evt.type;
-    console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-    console.log('Webhook body:', body)
+ 
+    
+    if (eventType === "user.created") {
+        await db.user.create({
+            data: {
+                externalUserId: payload.data.id,
+                username: payload.data.username,
+                imageUrl: payload.data.image_url,
+            }
+        })
+    }
 
+    if (eventType === "user.updated") {
+        const currentUser = await db.user.findUnique({
+            where: {
+                externalUserId: payload.data.id
+            }
+        });
+
+        if (!currentUser) {
+            return new Response("User not found", { status: 404 });
+        }
+
+        await db.user.update({
+            where: {
+                externalUserId: payload.data.id
+            },
+            data: {
+                username: payload.data.username,
+                imageUrl: payload.data.image_url,
+            }
+        });
+    }
+
+    if (eventType === "user.deleted") {
+        await db.user.delete({
+            where: {
+                externalUserId: payload.data.id
+            }
+        })
+    }
     return new Response('', { status: 200 })
 }
